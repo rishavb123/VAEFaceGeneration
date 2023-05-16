59767891e7a8bb6faf95d2d9acdb6497c7496628
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-05-16 19:59:26 -0400

    added grid view

--- vae/visualize.py
+++ vae/visualize.py
@@ -10,8 +10,14 @@ import cv2
 gray = False
 latent_dim = 100
 load_eps = False
+row, col = 4, 8
+img_size = 256
+gray_divider = 15
+color_divider = 10
+# Set row, col to 1, 1 to enable saving
 
 
+full_num = row * col
 latent_dim = latent_dim if gray else 3 * latent_dim
 
 model = CVAE(latent_dim, color=not gray)
@@ -24,23 +30,37 @@ model.set_weights(saved_model.get_weights())
 
 eps_names = list(os.listdir(f"{prepath}/eps"))
 if len(eps_names) == 0 or not load_eps:
-    eps = tf.random.normal(shape=(1, latent_dim))
+    eps = tf.random.normal(shape=(full_num, latent_dim))
 else:
-    eps = tf.convert_to_tensor(np.load(f"{prepath}/eps/{eps_names[-1]}"))
-divider = 15 if gray else 10
+    eps = tf.convert_to_tensor(
+        np.repeat(np.load(f"{prepath}/eps/{eps_names[-1]}"), full_num, axis=0)
+    )
+divider = gray_divider if gray else color_divider
 
 while True:
-    d_eps = tf.random.normal(shape=(1, latent_dim)) / divider
+    d_eps = tf.random.normal(shape=(full_num, latent_dim)) / divider
     eps += d_eps
 
-    img = model.sample(eps)[0].numpy()
+    results = model.sample(eps).numpy()
 
-    img = cv2.resize(img, (512, 512))
+    imgs = []
+
+    for r in range(row):
+        imgs.append([])
+        for c in range(col):
+            i = r * col + c
+            imgs[-1].append(results[i])
+
+    imgs = [np.concatenate(img_row, axis=1) for img_row in imgs]
+
+    img = np.concatenate(imgs, axis=0)
+
+    img = cv2.resize(img, (img_size * col, img_size * row))
 
     cv2.imshow("VAE", img)
 
     if cv2.waitKey(1) == ord("q"):
         break
 
-    if cv2.waitKey(1) == ord("s"):
+    if cv2.waitKey(1) == ord("s") and row == 1 and col == 1:
         np.save(f"{prepath}/eps/eps_{int(time.time())}.npy", eps.numpy())
